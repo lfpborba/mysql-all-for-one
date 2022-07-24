4fde9ab9d90c2be5771937d876c238df9b2d2245
lfpborba <luisfpb@icloud.com>
2022-07-23 22:38:26 -0300

    ...

--- desafio19.sql
+++ desafio19.sql
@@ -1,2 +1,2 @@
-SELECT COUNT(*) AS order_count FROM orders
-WHERE (employee_id = 5 OR employee_id = 6) AND shipper_id = 2;
+SELECT COUNT(employee_id) AS order_count FROM northwind.orders
+WHERE employee_id IN(5,6) AND shipper_id = 2;
